0ffc22aeeb846b9ee6eac421fdd48fdec7926d5f
Humberto Felipe Neri Neto <humbertofelipeneri@hotmail.com>
2023-08-01 16:09:31 -0300

    Repetições JS

---
+++ aula15/ambiente.js
@@ -0,0 +1,17 @@
+var c = 0
+while (c < 6) {
+    console.log(c + ' Olá, tudo bem?')
+    c++
+}
+
+var j = 0 
+do {
+    console.log(j + ' Olá, tudo bem?')
+    j++
+} while (j != 3)
+
+console.log('VAI COMEÇAR!!!')
+for (var i = 0 ; i < 3; i++) {
+    console.log(i);
+}
+console.log('FIM!!!')
